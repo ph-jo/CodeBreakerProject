2f6162d88867ed289a08b6125dec592384584203
Ph <Phogue-@live.com>
2018-02-05 16:58:21 +0100

    54

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,11 +2,26 @@ var answer = document.getElementById('answer');
 var attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
-    //add functionality to guess function here
-    if(answer.value == '' || attempt.value == ''){
+    var input = document.getElementById('user-guess');
+//add functionality to guess function here
+
+    if (answer.value == '' || attempt.value == '') {
         setHiddenFields();
     }
+
+    if (!validateInput(input.valueOf())) return;
+    attempt.value++;
+    if (getResults(i.value)) {
+        setMessage("You Win! :)");
+        showAnswer(true);
+        showReplay();
+    }else if(attempt.value >= 10) {
+        setMessage("You Lose! :(");
+        showAnswer(false);
+        showReplay();
+    }else{
+        setMessage("Incorrect, try again.");
+
 }
 
 //implement new functions here
@@ -29,4 +44,43 @@ function validateInput(input){
         return false;
     }
     return true;
+}
+
+
+function getResults(input){
+    var html = '<div class="row"><span class ="col-md-6">' + input + '</span><div class="col-md-6">';
+    for(i = 0 i < input.length; i++){
+        if(input.charAt(i) == answer.value.charAt(i)){
+            html += <span class="glyphicon glyphicon-ok"></span>;
+        }
+        else if(answer.value.indexOf(input.charAt(i)) > -1){
+            html += <span class="glyphicon glyphicon-transfer"></span>;
+        }else{
+            html += <span class="glyphicon glyphicon-remove"></span>;
+        }
+
+    }
+    html += '</div></div>';
+    document.getElementById('results').innerHTML += html;
+
+    if(input == answer.value)
+        return true;
+    }
+    return false;
+}
+
+function showAnswer(success){
+    var code = document.getElementById("code");
+    if(success) {
+        code.className += ' success';
+    }else{
+        code.className += ' failure';
+    }
+   code.innerHTML = answer.value;
+}
+
+
+function showReplay(){
+    document.getElementById('guessing-div').style.display = "none";
+    document.getElementById('replay-div').style.display = "block";
 }
