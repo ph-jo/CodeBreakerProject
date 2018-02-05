07a14d3c81e1e29f1a96942da29575505692dcdb
Ph <Phogue-@live.com>
2018-02-05 16:39:01 +0100

    3

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,15 +4,29 @@ var attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer.value == '' || attempt.value == ''){
+        setHiddenFields();
+    }
 }
 
 //implement new functions here
 
 function setHiddenFields(){
-    attempt = 0;
-    answer = Math.floor(Math.random()*Math.floor(9999));
-    while(answer.toString().length<4){
-        answer = "0" + answer;
+    attempt.value = 0;
+    answer.value = Math.floor(Math.random()*10000).toString();
+    while(answer.value.length<4){
+        answer.value = "0" + answer.value;
     }
 }
 
+function setMessage(message){
+    document.getElementById("message").innerHTML = message;
+}
+
+function validateInput(input){
+    if(input.length != 4){
+        setMessage('Guesses must be exactly 4 characters long');
+        return false;
+    }
+    return true;
+}
